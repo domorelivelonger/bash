a01847b59a0bb2422acdbbcc3437a151f2fe386c
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2020-03-03 17:22:46 +0000

    Create create-ipv6-proxy.sh

---
+++ ipv6-proxies/create-ipv6-proxy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright
+# Boris O. Baktashev
+# http://kak-podnyat-proksi-ipv6.ru
+# 2016
+
+
+ipv4=100.100.100.100
+portproxy=30000
+user=test
+pass=testtest
+config="/root/3proxy/3proxy.cfg"
+
+
+echo "daemon" >> $config
+echo "maxconn 1000" >> $config
+echo "nserver 2001:4860:4860::8844" >> $config
+echo "nserver 2001:4860:4860::8888" >> $config
+echo "nserver 8.8.8.8" >> $config
+echo "nscache 65536" >> $config
+echo "timeouts 1 5 30 60 180 1800 15 60" >> $config
+echo "setgid 65535" >> $config
+echo "setuid 65535" >> $config
+echo "flush" >> $config
+echo "auth strong" >> $config
+echo  "users $user:CL:$pass" >> $config
+echo "allow $user" >> $config
+
+for i in `cat ip.list`; do
+    echo "proxy -n -a -6 -p$portproxy -i$ipv4 -e$i" >> $config
+    ((inc+=1))
+    ((portproxy+=1))
+	echo "$ipv4:$portproxy:$user:$pass" >> proxylist.txt
+done
