26e9d2f1b8f719b0f3d9e4e90ea6827cbd6f3e4c
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2020-10-18 02:41:49 +0300

    Update 1-ubuntu-20-install.sh

--- 1-ubuntu-20-install.sh
+++ 1-ubuntu-20-install.sh
@@ -1,4 +1,4 @@
-apt update && apt upgrade -y && apt install htop wget mc curl pwgen ncdu screen nmap httping -y
+apt update && apt upgrade -y && apt install htop wget mc curl pwgen ncdu screen nmap httping net-tools redir -y
 
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
