beecdf5c3d8770c1570ff13800eaddf4da0b86af
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2021-07-09 17:10:47 +0000

    Create docker_docker_compose.sh

---
+++ docker_docker_compose.sh
@@ -0,0 +1,6 @@
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
