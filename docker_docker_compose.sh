d4d72ef542c0f3a47390c65dbb26e19c04a94a2a
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2021-07-09 17:21:10 +0000

    Update docker_docker_compose.sh

--- docker_docker_compose.sh
+++ docker_docker_compose.sh
@@ -1,5 +1,5 @@
-rm /usr/local/bin/docker-compose
-rm /usr/bin/docker-compose
+rm -f /usr/local/bin/docker-compose
+rm -f /usr/bin/docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
