19bd94882e5ae954a82fbe04219acfa6c7a3b458
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2021-07-31 19:14:30 +0000

    Update pptpd-install-debian-ubuntu.sh

--- proxies/pptpd/pptpd-install-debian-ubuntu.sh
+++ proxies/pptpd/pptpd-install-debian-ubuntu.sh
@@ -1,79 +1,35 @@
 #!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-export PATH
+apt install pptpd ppp iptables iproute2 -y && \
+    echo 'option /etc/ppp/pptpd-options' > /etc/pptpd.conf && \
+    echo 'pidfile /var/run/pptpd.pid' >> /etc/pptpd.conf && \
+    echo 'localip 192.168.47.1' >> /etc/pptpd.conf && \
+    echo 'remoteip 192.168.47.100-199' >> /etc/pptpd.conf && \
+    echo 'ms-dns 8.8.8.8\nms-dns 8.8.4.4' >> /etc/ppp/pptpd-options && \
+    echo 'nameserver 8.8.8.8\nameserver 8.8.4.4' > /etc/resolv.conf  && \
+    echo 'name pptpd' > /etc/ppp/pptpd-options && \
+    echo 'refuse-pap' >> /etc/ppp/pptpd-options && \
+    echo 'refuse-chap' >> /etc/ppp/pptpd-options && \
+    echo 'refuse-mschap' >> /etc/ppp/pptpd-options && \
+    echo 'require-mschap-v2' >> /etc/ppp/pptpd-options && \
+    echo 'require-mppe-128' >> /etc/ppp/pptpd-options && \
+    echo 'proxyarp' >> /etc/ppp/pptpd-options && \
+    echo 'nodefaultroute' >> /etc/ppp/pptpd-options && \
+    echo 'lock' >> /etc/ppp/pptpd-options && \
+    echo 'nobsdcomp' >> /etc/ppp/pptpd-options && \
+    echo 'novj' >> /etc/ppp/pptpd-options && \
+    echo 'novjccomp' >> /etc/ppp/pptpd-options && \
+    echo 'nologfd' >> /etc/ppp/pptpd-options
 
-clear
-CUR_DIR=$(pwd)
 
-if [ $(id -u) != "0" ]; then
-    printf "Error: You must be root to run this script!"
-    exit 1
-fi
-
-echo "#############################################################"
-echo "# PPTP VPN Auto Install"
-echo "# Env: Debian/Ubuntu"
-echo "# Created by Domorelivelonger on 2021.07.31"
-echo "# Author Url: http://webxdata.com"
-echo "# Version: 1.0"
-echo "#############################################################"
-echo ""
-
-apt-get -y update
-apt-get -y install pptpd
-
-cat >>/etc/pptpd.conf<<EOF
-localip 10.10.10.1
-remoteip 10.10.10.2-254
-EOF
-
-cp /etc/ppp/pptpd-options /etc/ppp/pptpd-options.old
-
-cat >/etc/ppp/pptpd-options<<EOF
-name pptpd
-refuse-pap
-refuse-chap
-refuse-mschap
-require-mschap-v2
-require-mppe-128
-ms-dns 8.8.8.8
-ms-dns 8.8.4.4
-proxyarp
-debug
-dump
-lock
-nobsdcomp
-novj
-novjccomp
-logfile /var/log/pptpd.log
-EOF
-
-cat >>/etc/ppp/chap-secrets<<EOF
-test * test *
-EOF
+echo 'test * test *' > /etc/ppp/chap-secrets
 
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sysctl -p
 
-
-iptables -t nat -A POSTROUTING -s 10.10.10.10/8 -j MASQUERADE
-iptables -I FORWARD -p tcp --syn -i ppp+ -j TCPMSS --set-mss 1300
-
-iptables-save > /etc/iptables.up.rules
-
-
-/etc/init.d/pptpd restart
-
-
-
-ufw allow 1723
-cp /etc/sysctl.conf /etc/sysctl.conf.original
-sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
-sysctl -p
-
-iptables -I INPUT -p tcp --dport 1723 -m state --state NEW -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -I INPUT -p gre -j ACCEPT
-iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
-iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -s 10.10.10.10/24 -j TCPMSS  --clamp-mss-to-pmtu
-iptables -I INPUT -s 10.10.10.0/24 -i ppp0 -j ACCEPT
+iptables -I INPUT -p tcp --dport 1723 -m state --state NEW -j ACCEPT
 iptables-save > /etc/iptables.up.rules
+ufw allow 1723
+systemctl restart ufw
+/etc/init.d/pptpd restart
