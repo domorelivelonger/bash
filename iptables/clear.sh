94766f4aecdac5b785a98fc366cd9e4f6c1abcc3
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2021-08-03 16:06:02 +0000

    Create clear.sh

---
+++ iptables/clear.sh
@@ -0,0 +1,9 @@
+#Clear all rules
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+#-------------
