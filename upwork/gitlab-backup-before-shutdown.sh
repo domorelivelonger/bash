85999e0694348c396c59e499ffa70ed9624b902f
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2021-08-12 17:05:14 +0000

    Update gitlab-backup-before-shutdown.sh

--- upwork/gitlab-backup-before-shutdown.sh
+++ upwork/gitlab-backup-before-shutdown.sh
@@ -12,6 +12,26 @@ EOF
 #3. make file executable
 chmod +x /root/backup_work.sh
 #4. change gitlab service systemd file /usr/lib/systemd/system/gitlab-runsvdir.service with timeout up to 20 minutes for make full backup
+cat <<EOF > /usr/lib/systemd/system/gitlab-backup.service
+[Unit]
+Description=Service to run on shutdown before any other services
+After=multi-user.target
+Before=gitlab-runsvdir.service
+Before=shutdown.target 
+
+[Service]
+Type=oneshot
+ExecStart=/bin/true
+ExecStop=/root/backup_work.sh
+Restart=always
+TasksMax=4915
+RemainAfterExit=yes
+TimeoutSec=1200
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#5. Restore Gitlab service file to default
 cat <<EOF > /usr/lib/systemd/system/gitlab-runsvdir.service
 [Unit]
 Description=GitLab Runit supervision process
@@ -21,11 +41,11 @@ After=multi-user.target
 ExecStart=/opt/gitlab/embedded/bin/runsvdir-start
 Restart=always
 TasksMax=4915
-ExecStop=bash /root/backup_work.sh
-TimeoutSec=1200
 
 [Install]
 WantedBy=multi-user.target
 EOF
-#5. reload daemon 
+
+#6. reload daemon 
 systemctl daemon-reload
+systemctl enable gitlab-backup
